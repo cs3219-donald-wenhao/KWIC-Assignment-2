6e6c2d0c39fc899024c99470785427ebd21e3b34
wenhaohao <lwen.hao93@gmail.com>
2016-09-09 16:39:57 +0800

    Added implementation of RequiredWord class

--- src/main/java/CS3213/RequiredWords.java
+++ src/main/java/CS3213/RequiredWords.java
@@ -2,9 +2,6 @@ package main.java.CS3213;
 
 import java.util.HashSet;
 
-/**
- * Created by junchao on 8/23/2014.
- */
 public class RequiredWords {
     private HashSet<String> _requiredWords;
     private static RequiredWords _instatnce;
@@ -13,20 +10,29 @@ public class RequiredWords {
     }
 
     public static RequiredWords getRequiredWords() {
-    	/* To be implemented */
-    	return new RequiredWords();
+        if (_instatnce == null) {
+            _instatnce = new RequiredWords();
+        }
+
+        return _instatnce;
     }
 
     public void addRequiredWord(String word) {
-        /* To be implemented */
+        assert(word != null);
+		this._requiredWords.add(word);
     }
 
     public void removeRequiredWord(String word) {
-    	/* To be implemented */
+        assert(word != null);
+        this._requiredWords.remove(word);
     }
 
     public boolean isRequiredWord(String word) {
-    	/* To be implemented */
-    	return false;
+        assert(word != null);
+        return this._requiredWords.contains(word);
+    }
+    
+    public boolean hasNoRequiredWords() {
+    	return this._requiredWords.isEmpty();
     }
 }
